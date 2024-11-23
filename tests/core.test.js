690943f323133eb23a276cf8573b847ea4474b72
Mehdi <mahditajikkazemii@gmail.com>
2024-11-24 01:50:38 +0330

    Testing a Stack DS class

--- tests/core.test.js
+++ tests/core.test.js
@@ -4,7 +4,8 @@ import {
     validateUserInput,
     isValidUsername,
     canDrive,
-    isPriceInRange } from '../src/core';
+    isPriceInRange,
+    Stack } from '../src/core';
 
 describe('getCoupons', () => {
     it('should return an array of coupons', () => {
@@ -127,5 +128,73 @@ describe('getCoupons', () => {
       { scenario: 'price > max', price: 200, result: false }
     ])('should return $result when $scenario', ({ price, result }) => {
       expect(isPriceInRange(price, 0, 100)).toBe(result);
+    });
+  });
+
+  describe('Stack', () => {
+    let stack;
+  
+    beforeEach(() => {
+      stack = new Stack();
+    });
+  
+    it('push should add an item to the stack', () => {
+      stack.push(1);
+  
+      expect(stack.size()).toBe(1);
+    });
+  
+    it('pop should remove and return the top item from the stack', () => {
+      stack.push(1);
+      stack.push(2);
+  
+      const poppedItem = stack.pop();
+  
+      expect(poppedItem).toBe(2);
+      expect(stack.size()).toBe(1);
+    });
+  
+    it('pop should throw an error if stack is empty', () => {
+      expect(() => stack.pop()).toThrow(/empty/i);
+    });
+  
+    it('peek should return the top item from the stack without removing it', () => {
+      stack.push(1);
+      stack.push(2);
+  
+      const peekedItem = stack.peek();
+  
+      expect(peekedItem).toBe(2);
+      expect(stack.size()).toBe(2);
+    });
+  
+    it('peek should throw an error if stack is empty', () => {
+      expect(() => stack.peek()).toThrow(/empty/i);
+    });
+  
+    it('isEmpty should return true if stack is empty', () => {
+      expect(stack.isEmpty()).toBe(true);
+    });
+  
+    it('isEmpty should return false if stack is not empty', () => {
+      stack.push(1);
+  
+      expect(stack.isEmpty()).toBe(false);
+    });
+  
+    it('size should return the number of items in the stack', () => {
+      stack.push(1);
+      stack.push(2);
+  
+      expect(stack.size()).toBe(2);
+    });
+  
+    it('clear should remove all items from the stack', () => {
+      stack.push(1);
+      stack.push(2);
+  
+      stack.clear();
+  
+      expect(stack.size()).toBe(0);
     });
   });
