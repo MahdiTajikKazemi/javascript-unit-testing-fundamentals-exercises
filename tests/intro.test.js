bf31d54627424eb3a9ce4cd77e656ae42762e309
Mehdi <mahditajikkazemii@gmail.com>
2024-11-24 01:11:07 +0330

    Build factorial function using TDD

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -18,3 +18,29 @@ describe('fizzBuzz', () => {
       expect(fizzBuzz(1)).toBe('1');
     });
   });
+
+  describe('factorial', () => {
+    it('should return 1 if given 0', () => {
+      expect(factorial(0)).toBe(1);
+    });
+  
+    it('should return 1 if given 1', () => {
+      expect(factorial(1)).toBe(1);
+    });
+  
+    it('should return 2 if given 2', () => {
+      expect(factorial(2)).toBe(2);
+    });
+  
+    it('should return 6 if given 3', () => {
+      expect(factorial(3)).toBe(6);
+    });
+  
+    it('should return 24 if given 4', () => {
+      expect(factorial(4)).toBe(24);
+    });
+  
+    it('should return undefined if given a negative number', () => {
+      expect(factorial(-1)).toBeUndefined();
+    });
+  });
