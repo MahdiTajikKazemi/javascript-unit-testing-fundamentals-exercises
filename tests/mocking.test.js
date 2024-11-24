b32b4a9f5ccceec63e46750b3c6c14a6f0bd2774
Mehdi <mahditajikkazemii@gmail.com>
2024-11-24 17:53:21 +0330

    Testing submitOrder & getDiscount functions

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,8 +1,12 @@
 import { vi, it, expect, describe } from 'vitest';
-import { getShippingInfo } from '../src/mocking';
+import { getShippingInfo, submitOrder, getDiscount} from '../src/mocking';
+
 import { getShippingQuote } from '../src/libs/shipping';
+import { charge } from '../src/libs/payment';
 
 vi.mock('../src/libs/shipping');
+vi.mock('../src/libs/payment');
+
 
 describe('test suite', () => {
     it('test case', () => {
@@ -39,3 +43,51 @@ describe('test suite', () => {
       expect(result).toMatch(/shipping cost: \$10 \(2 days\)/i);
     });
   });
+
+  describe('submitOrder', () => {
+    const order = { totalAmount: 10 };
+    const creditCard = { creditCardNumber: '1234' };
+  
+    it('should charge the customer', async () => {
+      vi.mocked(charge).mockResolvedValue({ status: 'success' });
+  
+      await submitOrder(order, creditCard);
+  
+      expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
+    });
+  
+    it('should return success when payment is successful', async () => {
+      vi.mocked(charge).mockResolvedValue({ status: 'success' });
+  
+      const result = await submitOrder(order, creditCard);
+  
+      expect(result).toEqual({ success: true });
+    });
+  
+    it('should return success when payment is successful', async () => {
+      vi.mocked(charge).mockResolvedValue({ status: 'failed' });
+  
+      const result = await submitOrder(order, creditCard);
+  
+      expect(result).toEqual({ success: false, error: 'payment_error' });
+    });
+  });
+
+  describe('getDiscount', () => {
+    it('should return .2 on Christmas day', () => {
+      vi.setSystemTime('2024-12-25 00:01');
+      expect(getDiscount()).toBe(0.2);
+  
+      vi.setSystemTime('2024-12-25 23:59');
+      expect(getDiscount()).toBe(0.2);
+    });
+  
+    it('should return 0 on any other day', () => {
+      vi.setSystemTime('2024-12-24 00:01');
+      expect(getDiscount()).toBe(0);
+  
+      vi.setSystemTime('2024-12-26 00:01');
+      expect(getDiscount()).toBe(0);
+    });
+  });
+  
